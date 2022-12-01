31cd1f0b73867aa67cdd9a41258a04494507f17e
SANOGOTECH <sanogotech@yahoo.fr>
2022-12-01 11:21:06 +0000

    Update mysamplejoin.py

--- joindata/mysamplejoin.py
+++ joindata/mysamplejoin.py
@@ -11,4 +11,20 @@ from pyspark.sql.functions import col
 
 spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
 
+## https://www.learnbymarketing.com/1100/pyspark-joins-by-example/
 
+# Setting up the Data in Pyspark
+
+valuesA = [('Pirate',1),('Monkey',2),('Ninja',3),('Spaghetti',4)]
+TableA = spark.createDataFrame(valuesA,['name','id'])
+ 
+valuesB = [('Rutabaga',1),('Pirate',2),('Ninja',3),('Darth Vader',4)]
+TableB = spark.createDataFrame(valuesB,['name','id'])
+ 
+TableA.show()
+TableB.show()
+
+ta = TableA.alias('ta')
+tb = TableB.alias('tb')
+
+# Pyspark Inner Join Example
